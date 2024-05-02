cacfe99ed1ce22eae37c9e2dbb27b9e6b39b36d6
matianxiang <2950208312@qq.com>
2024-05-02 18:53:31 +0800

    feat: 配置upload模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InusController } from './inus/inus.controller';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [],
+  imports: [UploadModule], //将upload模块导入根模块
   controllers: [AppController, InusController],
   providers: [AppService],
 })
