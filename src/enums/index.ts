dd6c1efb473ad9ae880e0355a8b61b81f0c68ca6
matianxiang <2950208312@qq.com>
2024-05-06 20:38:01 +0800

    update: 更新http code 枚举

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,6 +1,9 @@
 export enum HttpCode {
-  OK=200,
-  REDIRECT=302,
-  FAILED=400,
-  UNAUTHORIZED=403,
+  OK = 200,
+  REDIRECT = 302,
+  FAILED = 400, // 参数错误
+  UNAUTHORIZED = 401, //未授权
+  FORBIDDEN = 403, //验证失败
+  NOT_FOUND = 404, //接口不存在
+  INTERNAL_SERVER_ERROR = 500, //服务端错误
 }
