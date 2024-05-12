69f8b92ad413d109875564c094020c1dc3f9e633
matianxiang <2950208312@qq.com>
2024-05-12 20:14:05 +0800

    feat: 封装CacheInterceptor

---
+++ src/interceptors/cache-interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class CacheInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      tap(() => {
+        const response = context.switchToHttp().getResponse();
+        // 缓存时间 3600s 1h内不请求服务器
+        response.set('Cache-Control', 'private, max-age=3600');
+      }),
+    );
+  }
+}
