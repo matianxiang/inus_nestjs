64d98ba8f283c3cbe9bdc28b8f93609b2aab63e7
matianxiang <2950208312@qq.com>
2024-05-12 20:41:02 +0800

    feat: 生成user模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { InusController } from './inus/inus.controller';
 import { UploadModule } from './upload/upload.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UploadModule], //将upload模块导入根模块
+  imports: [UploadModule, UserModule], //将upload模块导入根模块
   controllers: [AppController, InusController],
   providers: [AppService],
 })
