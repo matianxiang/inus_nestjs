a7d058fd4a525a945d1f595541162ce613a02fa3
matianxiang <2950208312@qq.com>
2024-05-15 23:51:00 +0800

    chore: 开启typeorm日志

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,6 +21,7 @@ import { Follow } from './follow/entities/follow.entity';
       username: 'inus',
       password: '123456',
       database: 'inus',
+      logging: true, // 开启日志
       entities: [User, Follow], // 用于指定应用中所有数据库实体的位置。实体（Entity）是 TypeORM 用来映射数据库表到 JavaScript 或 TypeScript 类的一种机制。每一个实体类代表数据库中的一张表，类中的属性对应于表中的列。
       synchronize: true, // 控制是否允许 TypeORM 根据实体自动更新数据库结构。
     }),
